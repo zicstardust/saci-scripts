bee3cdd4ef2f987ef8d00d69f21bae322fa7c103
zic <git@zicstardust.com>
2023-11-25 19:41:38 -0400

    remove git update

--- scripts/system-update/system-update.sh
+++ scripts/system-update/system-update.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-config_file=${script_dir}/system-update.config
-source $config_file
+#script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#config_file=${script_dir}/system-update.config
+#source $config_file
 
 start(){
     echo "---------------Start update---------------"
     apt update -y
     apt upgrade -y
     apt autoremove -y
-
-    directories=$(ls ${REPOS_DIR})
-    for i in ${directories}; do
-        sudo su -c "git -C ${REPOS_DIR}/${i} pull" ${OS_USER}
-    done
     echo "---------------End update-----------------"
 }
 
@@ -22,22 +17,12 @@ gen-config-file(){
         cat ${config_file} 1> ${config_file}_old
     fi
     cat > ${config_file} <<EOF
-OS_USER=
-REPOS_DIR=
 
 EOF
 }
 
 check_and_run (){
-    if [ -z ${REPOS_DIR} ]; then
-        echo "REPOS_DIR not declared in config file"
-        exit 2
-    elif [ -z ${OS_USER} ]; then
-        echo "OS_USER not declared in config file"
-        exit 2
-    else
-        start
-    fi
+    start    
 }
 
 if [ "${1}" == "gen-config-file" ]; then
