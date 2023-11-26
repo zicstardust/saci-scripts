65fa2b78e54cdde2346b67e8805529ed0c565c3a
zic <git@zicstardust.com>
2023-11-25 20:00:16 -0400

    fix: install

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ install(){
     git clone https://github.com/zicstardust/saci-scripts.git
     scripts=$( ls /tmp/saci-scripts/scripts )
     for i in $scripts; do
-        mv ${i}/${i}.service /etc/systemd/system/
-        mv ${i}/${i}.timer /etc/systemd/system/
-        mv ${i}/${i}.sh /etc/systemd/
+        mv saci-scripts/scripts/${i}/${i}.service /etc/systemd/system/
+        mv saci-scripts/scripts/${i}/${i}.timer /etc/systemd/system/
+        mv saci-scripts/scripts/${i}/${i}.sh /etc/systemd/
         chmod +x /etc/systemd/${i}.sh
     done
     rm -Rf /tmp/saci-scripts
